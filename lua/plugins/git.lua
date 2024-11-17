cacc39c6a557605572aeb20021cecbf03ce4d6a2
Zkrallah <muhammad.heshamyt@gmail.com>
2024-11-17 19:46:57 +0200

    feat: Lazygit plugin initial config.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,5 +17,26 @@ return {
             vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>',
                 { desc = "toggle blame", noremap = true, silent = true })
         end
+    },
+    -- nvim v0.8.0
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
     }
 }
