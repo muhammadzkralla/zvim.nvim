14db1a882bcf42bf16a743dc2c40aa1236c04d81
Zkrallah <muhammad.heshamyt@gmail.com>
2024-11-28 17:25:56 +0200

    docs: New changes addressed in images.

--- lua/plugins/zpragmatic.lua
+++ lua/plugins/zpragmatic.lua
@@ -5,19 +5,10 @@ return {
         require("zpragmatic").setup({
             filetype_questions = {
                 ["*"] = {
-                    "Don't break any windows.",
-                    "Don't violate the DRY principle.",
-                    "Comment your code."
                 },
                 ["java"] = {
-                    "All maven tests must pass before you save.",
-                    "Check for potential null pointer exceptions.",
-                    "All public methods should have proper documentation.",
                 },
                 ["javascript"] = {
-                    "Run the linter.",
-                    "Test the code in multiple browsers if necessary.",
-                    "Ensure there are no console logs left in the code.",
                 },
             },
             bypass_filetypes = { "markdown", "txt", "text", "plain", "json" },
