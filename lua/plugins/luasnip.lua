89e8e80aa05549ba594c7d96435ea2cf1b6f8b25
Zkrallah <muhammad.heshamyt@gmail.com>
2024-12-09 21:34:56 +0200

    feat: Added a new custom snippet in luasnip.

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,7 +1,7 @@
 return {
     "L3MON4D3/LuaSnip",
     -- follow latest release.
-    version = "v2.*",     -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
 
@@ -111,6 +111,27 @@ return {
                 t("n /= k;"), t({ "", "\t" }),
                 t("}"), t({ "", "\t" }),
                 t("return result;"), t({ "", "}" })
+            }),
+
+            -- sieve function
+            s("sieve", {
+                t({
+                    "void sieve(ll n) {",
+                    "    low.assign(n + 1, 0);",
+                    "",
+                    "    for (int i = 2; i <= n; ++i) {",
+                    "        if (!low[i]) {",
+                    "            low[i] = i;",
+                    "            pr.push_back(i);",
+                    "        }",
+                    "",
+                    "        for (int &j: pr) {",
+                    "            if (j > low[i] || i * j > n) break;",
+                    "            low[j * i] = j;",
+                    "        }",
+                    "    }",
+                    "}",
+                }),
             }),
         })
     end,
