1e186d8a1294709d9c83821c6809f79ea4ef95d0
zkrallah <muhammad.heshamyt@gmail.com>
2025-07-02 21:06:28 +0300

    feat: Implemented presence.nvim plugin.

---
+++ lua/plugins/discord.lua
@@ -0,0 +1,24 @@
+return {
+    "andweeb/presence.nvim",
+    event = "VeryLazy",
+    config = function()
+        require("presence").setup({
+            auto_update        = true,
+            neovim_image_text  = "I'm Using NeoVim, btw",
+            main_image         = "neovim",
+            log_level          = nil,
+            debounce_timeout   = 10,
+            enable_line_number = false,
+            buttons            = false,
+            show_time          = true,
+            idle_text          = "Browsing the dashboard...",
+        })
+
+        -- vim.api.nvim_create_autocmd("User", {
+        --     pattern = "DashboardReady",
+        --     callback = function()
+        --         require("presence"):update()
+        --     end,
+        -- })
+    end,
+}
