cee5e104d90852dfe4c9ece25e90e6b43f5ee581
zkrallah <muhammad.heshamyt@gmail.com>
2025-11-05 19:30:54 +0200

    style: New colorscheme, alabaster.nvim.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,18 +5,18 @@ return {
     -- { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
     {
         "navarasu/onedark.nvim",
-        config = function ()
+        config = function()
             -- Lua
             require('onedark').setup {
                 -- Main options --
-                style = 'deep',   -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-                transparent = true, -- Show/hide background
-                term_colors = true, -- Change terminal color as per the selected theme style
-                ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
+                style = 'deep',               -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+                transparent = true,           -- Show/hide background
+                term_colors = true,           -- Change terminal color as per the selected theme style
+                ending_tildes = true,         -- Show the end-of-buffer tildes. By default they are hidden
                 cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
                 -- toggle theme style ---
-                toggle_style_key = nil,                                                -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+                toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
                 toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
 
                 -- Change code style ---
@@ -36,13 +36,13 @@ return {
                 },
 
                 -- Custom Highlights --
-                colors = {}, -- Override default colors
+                colors = {},     -- Override default colors
                 highlights = {}, -- Override highlight groups
 
                 -- Plugins Config --
                 diagnostics = {
-                    darker = true, -- darker colors for diagnostic
-                    undercurl = true, -- use undercurl instead of underline for diagnostics
+                    darker = true,     -- darker colors for diagnostic
+                    undercurl = true,  -- use undercurl instead of underline for diagnostics
                     background = true, -- use background color for virtual text
                 },
             }
@@ -54,21 +54,21 @@ return {
         name = "rose-pine",
         config = function()
             require("rose-pine").setup({
-                variant = "moon", -- auto, main, moon, or dawn
-                dark_variant = "moon", -- main, moon, or dawn
+                variant = "main",      -- auto, main, moon, or dawn
+                dark_variant = "main", -- main, moon, or dawn
                 dim_inactive_windows = true,
-                extend_background_behind_borders = true,
+                extend_background_behind_borders = false,
 
                 enable = {
                     terminal = true,
                     legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-                    migrations = true, -- Handle deprecated options automatically
+                    migrations = true,        -- Handle deprecated options automatically
                 },
 
                 styles = {
                     bold = true,
                     italic = true,
-                    transparency = false,
+                    transparency = true,
                 },
 
                 groups = {
@@ -151,5 +151,13 @@ return {
             return plugin.load()
         end,
         lazy = false,
+    },
+    {
+        "p00f/alabaster.nvim",
+        config = function()
+            vim.g.alabaster_dim_comments = false
+            vim.g.alabaster_floatborder  = false
+            vim.cmd("colorscheme alabaster")
+        end
     }
 }
